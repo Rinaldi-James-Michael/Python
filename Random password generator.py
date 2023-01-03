8da661a907f023200d31cd07daa5605693ccb6f0
Rinaldi-James-Michael <64921836+Rinaldi-James-Michael@users.noreply.github.com>
2023-01-03 14:56:27 +0530

    Create Random password generator.py
    Version 1

---
+++ Random password generator.py
@@ -0,0 +1,12 @@
+import random
+
+passwords=""
+randomNumber = random.randrange(6,100,1)
+passwordCharacters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","!","@","%","^","&","*","(",")","-","+","=",".","/",">","<",";",":","|","?","~"]
+
+
+for p in range(randomNumber):
+   passwords = passwords+passwordCharacters[random.randrange(0,len(passwordCharacters),1)]
+
+ 
+print(passwords)
